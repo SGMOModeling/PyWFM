52d2ad30ec618a8da18600313cfedd9b8cd52308
Tyler Hatch <hatch.tyler@gmail.com>
2022-01-06 11:17:53 -0800

    Update get_values_for_some_zones_for_an_interval Fix bug in col_names

--- src/pywfm/zbudget.py
+++ src/pywfm/zbudget.py
@@ -868,14 +868,16 @@ class IWFMZBudget(IWFMMiscellaneous):
         values = np.array(zbudget_values)
 
         # get zone names
-        zone_names = self.get_zone_names()
+        zone_names = np.array(self.get_zone_names())
 
-        # convert output to a dictionary by zone name
+        # convert output to a dictionary of DataFrames by zone name
         value_dict = {}
-        for i, z in enumerate(np.array(zone_names)[np.array(zone_ids) - 1]):
-            col_names = np.array(column_name_array)[0, np.array(column_id_array) - 1]
-            df = pd.DataFrame(data=np.atleast_2d(values[i]), columns=col_names[i])
+        for i, z in enumerate(zone_names[np.array(zone_ids) - 1]):
+            col_names = np.array(column_name_array)[i, np.array(column_id_array)[i] - 1]
+            
+            df = pd.DataFrame(data=np.atleast_2d(values[i]), columns=col_names)
             df['Time'] = df['Time'].astype('timedelta64[D]') + np.array('1899-12-30', dtype='datetime64')
+            
             value_dict[z] = df
 
         return value_dict
