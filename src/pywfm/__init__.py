a442b41ec5f6ca5edb1a08cca2c041d9f4ac08ab
Tyler Hatch <hatch.tyler@gmail.com>
2022-03-17 15:25:09 -0700

    Add relative path to IWFM DLL This refers to the location where conda installs the IWFM DLL

--- src/pywfm/__init__.py
+++ src/pywfm/__init__.py
@@ -1,3 +1,8 @@
+import os
+
+DLL_PATH = os.path.normpath(os.path.join(__file__, '../../../../Library/bin'))
+DLL = 'IWFM2015_C_x64.dll'
+
 from pywfm.model import IWFMModel
 from pywfm.budget import IWFMBudget
 from pywfm.zbudget import IWFMZBudget
