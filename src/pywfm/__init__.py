f8dea4c440958d78b03bb08c23dac1e112df3e59
Tyler Hatch <hatch.tyler@gmail.com>
2022-06-13 16:08:34 -0700

    Update to load platform specific shared library

--- src/pywfm/__init__.py
+++ src/pywfm/__init__.py
@@ -1,7 +1,16 @@
 import os
+import platform
+import ctypes
 
-DLL_PATH = os.path.normpath(os.path.join(__file__, "../../../../Library/bin"))
-DLL = "IWFM2015_C_x64.dll"
+if platform.uname()[0] == "Windows":
+    DLL_PATH = os.path.normpath(os.path.join(__file__, "../../../../Library/bin"))
+    DLL = "IWFM2015_C_x64.dll"
+    LIB = ctypes.CDLL(os.path.join(DLL_PATH, DLL))
+elif platform.uname()[0] == "Linux":
+    SO_PATH = os.path.join(os.path.expanduser('~'), "iwfm-docker/build/iwfm")
+    SO = "libIWFMLib.so"
+    LIB = ctypes.CDLL(os.path.join(SO_PATH, SO))
+
 
 from pywfm.model import IWFMModel
 from pywfm.budget import IWFMBudget
