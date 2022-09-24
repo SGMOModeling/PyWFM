1b32c2844263e8ea55dd6a4ca722581ca3783f6e
Tyler Hatch <hatch.tyler@gmail.com>
2022-09-23 21:51:45 -0700

    Update version to 0.2.1

--- docsrc/conf.py
+++ docsrc/conf.py
@@ -23,7 +23,7 @@ copyright = '{}, California Department of Water Resources'.format(date.today().y
 author = 'Tyler Hatch'
 
 # The full version, including alpha/beta/rc tags
-release = '0.2.0'
+release = '0.2.1'
 
 
 # -- General configuration ---------------------------------------------------
